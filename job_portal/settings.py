96369b59582a7df3130aaee74ac9b0237d084694
gideonbms <61124278+gideonbms@users.noreply.github.com>
2020-02-21 22:20:08 +0000

    Update settings.py

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -146,14 +146,9 @@ USE_TZ = True
 #MEDIA_ROOT = os.path.join(BASE_DIR, "media")STATIC_URL = '/static/'
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'job_portal/static'),
 ]
-
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
 
 
 
